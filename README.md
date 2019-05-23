8df68efc4e47e18da9a23bfdc5d9c174856e3d1b
Kevin Alwell <alwell-kevin@github.com>
2019-05-23 15:16:03 -0400

    collect user input

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # tarball-getter-cli
 > Command Line utility that GETs your Repo from your Org as a tarball through CLI
+
+## USAGE
+
+**Installation**
+
+> Install
--- index.js
+++ index.js
@@ -3,3 +3,20 @@ const clear       = require('clear');
 const figlet      = require('figlet');
 
 const files = require('./lib/files');
+const inquirer = require('./lib/inquirer');
+
+const run = async () => {
+  clear();
+  
+  console.log(
+    chalk.yellow(
+      figlet.textSync('Tarball Getter', { horizontalLayout: 'full' })
+    )
+  );
+
+
+  const credentials = await inquirer.askGithubCredentials();
+  console.log("credentials: ", credentials);
+}
+
+run();
